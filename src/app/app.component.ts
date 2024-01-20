fe6e2bee9945c595f840d082e1a28e402d42fc99
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-01-20 15:32:30 +0100

    add upload ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CloudinaryModule } from '@cloudinary/ng';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CloudinaryModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cloudinary-angular-sdk';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'image',
+    title: 'Coudinary Image Module',
+    loadComponent: () =>
+      import('./images/images.component').then((m) => m.ImagesComponent),
+  },
+  {
+    path: 'video',
+    title: 'Coudinary Video Module',
+    loadComponent: () =>
+      import('./videos/videos.component').then((m) => m.VideosComponent),
+  },
+  {
+    path: 'upload',
+    title: 'Upload cloudinary assets',
+    loadComponent: () =>
+      import('./upload/upload.component').then((m) => m.UploadComponent),
+  },
+];
