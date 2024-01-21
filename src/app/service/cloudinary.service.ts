56b7e0bffa955aa229212fb08798b47eafdb137c
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-01-21 23:26:33 +0100

    add video component

--- src/app/service/cloudinary.service.ts
+++ src/app/service/cloudinary.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Cloudinary, CloudinaryImage, CloudinaryVideo } from '@cloudinary/url-gen';
+import {
+  Cloudinary,
+  CloudinaryImage,
+  CloudinaryVideo,
+} from '@cloudinary/url-gen';
 import { fill, thumbnail } from '@cloudinary/url-gen/actions/resize';
 import { byRadius } from '@cloudinary/url-gen/actions/roundCorners';
 import { Gravity } from '@cloudinary/url-gen/qualifiers';
 import { AutoFocus } from '@cloudinary/url-gen/qualifiers/autoFocus';
 import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn';
 
-interface ImageProperties {
+interface CloudinaryProperties {
   width: number | string;
   height: number | string;
   roundCorners?: boolean;
@@ -28,29 +32,38 @@ export class CloudinaryService {
     });
   }
 
-  loadImage(prop: ImageProperties) {
+  loadImage(prop: CloudinaryProperties) {
     this.img = this.cld.image('cld-sample-5');
     return this.img.resize(thumbnail().width(prop.width).height(prop.height)); // Crop the image, focusing on the face.
   }
 
-  changeRadius(prop: ImageProperties) {
+  changeRadius(prop: CloudinaryProperties) {
     this.img = this.cld.image('cld-sample-5');
     return this.img
       .resize(thumbnail().width(prop.width).height(prop.height))
       .roundCorners(byRadius(prop.radius)); // Crop the image, focusing on the face.
   }
 
-  loadVideo() {
+  loadVideo(props: CloudinaryProperties) {
+    this.vid = this.cld.video('Introduction-Santosh');
+    return this.vid.resize(
+      fill().width(props.width).height(props.height)
+      // .gravity(
+      //   Gravity.autoGravity().autoFocus(AutoFocus.focusOn(FocusOn.faces()))
+      // )
+    ); // Crop the video, focusing on the faces.
+    // .roundCorners(byRadius(20)); // Round the corners.
+  }
+
+  changeVideoRadius(props: CloudinaryProperties) {
     this.vid = this.cld.video('Introduction-Santosh');
     return this.vid
       .resize(
-        fill()
-          .width(150)
-          .height(150)
-          .gravity(
-            Gravity.autoGravity().autoFocus(AutoFocus.focusOn(FocusOn.faces()))
-          )
+        fill().width(props.width).height(props.height)
+        // .gravity(
+        //   Gravity.autoGravity().autoFocus(AutoFocus.focusOn(FocusOn.faces()))
+        // )
       ) // Crop the video, focusing on the faces.
-      .roundCorners(byRadius(20)); // Round the corners.
+      .roundCorners(byRadius(props.radius)); // Round the corners.
   }
 }
--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,20 +1,64 @@
 import { Component } from '@angular/core';
 import { CloudinaryModule } from '@cloudinary/ng';
-import { CloudinaryService } from '../service/cloudinary.service';
 import { CloudinaryVideo } from '@cloudinary/url-gen';
+import { JsonPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { CloudinaryService } from '../service/cloudinary.service';
+
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [CloudinaryModule],
+  imports: [
+    CloudinaryModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSliderModule,
+    FormsModule,
+    JsonPipe,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.scss',
 })
 export class VideosComponent {
   vid!: CloudinaryVideo;
+  width = '0';
+  height = 0;
+  rounded = false;
+  radius = 0;
+
   constructor(private readonly cludinaryService: CloudinaryService) {}
 
   ngOnInit() {
-    this.vid = this.cludinaryService.loadVideo();
+    this.vid = this.cludinaryService.loadVideo({
+      height: 200,
+      width: 200,
+    });
+  }
+
+  applyTrasformnation() {
+    this.vid = this.rounded
+      ? this.applyRadius()
+      : this.cludinaryService.loadVideo({
+          width: this.width,
+          height: this.height,
+        });
+  }
+
+  applyRadius() {
+    return this.cludinaryService.changeVideoRadius({
+      width: this.width,
+      height: this.height,
+      radius: this.radius,
+      roundCorners: this.rounded,
+    });
   }
 }
